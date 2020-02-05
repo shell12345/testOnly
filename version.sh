5498b452979a606d06ef8125df4b3b03faf275b2
shell12345 <46412851+shell12345@users.noreply.github.com>
2020-02-05 09:49:53 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ export LIBSODIUM_LINK="https://github.com/jedisct1/libsodium/releases/download/$
 export MBEDTLS_VER=2.16.3
 export MBEDTLS_LINK="https://tls.mbed.org/download/mbedtls-${MBEDTLS_VER}-gpl.tgz"
 # SS_LIBEV
-export SS_LIBEV_VER=3.3.3
+export SS_LIBEV_VER=3.3.4
 export SS_LIBEV_LINK="https://github.com/shadowsocks/shadowsocks-libev/releases/download/v${SS_LIBEV_VER}/shadowsocks-libev-${SS_LIBEV_VER}.tar.gz"
 export SS_LIBEV_YUM_INIT="https://raw.githubusercontent.com/shell12345/testOnly/master/ss_libev.init"
 export SS_LIBEV_APT_INIT="https://raw.githubusercontent.com/shell12345/testOnly/master/ss_libev_apt.init"
@@ -25,7 +25,7 @@ export SSRR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/akkariiin/
 export SSRR_YUM_INIT="https://raw.githubusercontent.com/shell12345/testOnly/master/ssrr.init"
 export SSRR_APT_INIT="https://raw.githubusercontent.com/shell12345/testOnly/master/ssrr_apt.init"
 # KCPTUN
-export KCPTUN_VER=20200103
+export KCPTUN_VER=20200201
 export KCPTUN_AMD64_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-amd64-${KCPTUN_VER}.tar.gz"
 export KCPTUN_386_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-386-${KCPTUN_VER}.tar.gz"
 export KCPTUN_INIT="https://raw.githubusercontent.com/shell12345/testOnly/master/kcptun.init"
